ed775512077eac375ff6b186ed5a00f56b61e27c
Sakshi Raj <118617849+isakshiraj@users.noreply.github.com>
2025-07-09 17:24:32 +0530

    changeENVfile

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-require("dotenv").config(); // Make sure this is at the top
+ 
+console.log("MONGO_URI:", process.env.MONGO_URI);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
 
